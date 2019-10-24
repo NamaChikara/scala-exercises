ce8bfd028c8a9469bcd447c91db6d16284678363
zack-barry-G5 <zack.barry@getg5.com>
2019-10-24 16:55:45 -0700

    simplified code

--- exercism/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ exercism/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,28 +1,23 @@
 object SumOfMultiples {
 
-  //https://stackoverflow.com/questions/26558120/may-a-while-loop-be-used-with-yield-in-scala
-
-  // iterate "generates the sequence resulting from the repeated application of a function to a start element."
-  // takeWhile is an iterator returning elements from the object it is called on as long as a condition is satisfied
-
-  // Note: could also do Seq.iterate(factor, len: Int){}.takeWhile(_ < 15), but need to know the length of the final
-  //       set in advance.
-
-  def get_multiples(factor: Int, limit: Int): Set[Int] = {
-
-    Iterator
-      .iterate(factor){ _ + factor }
-      .takeWhile(_ < limit)
-      .toSet
-
-  }
-
   def sum(factors: Set[Int], limit: Int): Int = {
 
-    val multiples = for (f <- factors) yield get_multiples(f, limit)
+    val multiples = for (f <- factors) yield {
+      Iterator
+        .iterate(f){ _ + f }
+        .takeWhile(_ < limit)
+        .toSet
+    }
 
     multiples.flatten.sum
 
   }
 }
 
+//https://stackoverflow.com/questions/26558120/may-a-while-loop-be-used-with-yield-in-scala
+
+// iterate "generates the sequence resulting from the repeated application of a function to a start element."
+// takeWhile is an iterator returning elements from the object it is called on as long as a condition is satisfied
+
+// Note: could also do Seq.iterate(factor, len: Int){}.takeWhile(_ < 15), but need to know the length of the final
+//       set in advance.

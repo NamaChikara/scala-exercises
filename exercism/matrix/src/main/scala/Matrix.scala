04aa449b5a2f80060895d83d3f93f3c24e05cf72
zack-barry-G5 <zack.barry@getg5.com>
2019-11-06 16:55:59 -0800

    improved solution

--- exercism/matrix/src/main/scala/Matrix.scala
+++ exercism/matrix/src/main/scala/Matrix.scala
@@ -1,34 +1,19 @@
 
 case class Matrix(input: String = "") {
 
-  val NEWLINE_REGEX = "\\n".r
-
-  val all_values = input
+  private val matrix_rows = input
     .split("\\n")
     .map(_.split(" "))
-    .flatten.map(_.toInt)
-    .zipWithIndex
-
-  val num_rows = NEWLINE_REGEX.findAllIn(input).length + 1
-
-  val num_cols = all_values.length / num_rows
 
   def row(r: Int): Vector[Int] = {
 
-    all_values
-      .filter(_._2 < (r + 1) * num_cols)
-      .filter(_._2 >= r * num_cols)
-      .map({ case(a, b) => a })
-      .toVector
+    matrix_rows(r).map(_.toInt).toVector
 
   }
 
   def column(c: Int): Vector[Int] = {
 
-    all_values
-      .filter(_._2 % num_cols == c)
-      .map({ case(a, b) => a})
-      .toVector
+    for (r <- matrix_rows.toVector) yield r(c).toInt
 
   }
 
